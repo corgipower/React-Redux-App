ad9d693039b3f7124369683db183222207a07e3f
Liz Shulman <corgipower@hotmail.com>
2020-10-20 20:55:00 -0400

    mvp

---
+++ got-quotes/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GotQuotes from './components/GotQuotes';
+
+function App() {
+  return (
+    <div className="App">
+      <GotQuotes />
+    </div>
+  );
+}
+
+export default App;
